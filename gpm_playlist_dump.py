10b38aea3e904bf6f3db7221441572a04cf9f0c3
Blake Irwin <blake@blake.kiwi>
2018-11-25 19:28:41 +1300

    Initial commit

---
+++ gpm_playlist_dump.py
@@ -0,0 +1,258 @@
+GIT_REPO_PATH = ''
+GIT_REPO_BRANCH = 'master'
+
+
+GOOGLE_USERNAME = ''
+GOOGLE_PASSWORD = ''
+GOOGLE_LOCALE = 'en_NZ'
+GOOGLE_DEVICE_ID = ''
+
+
+GPM_TRACK_FIELDS = [
+    'title', 'artist', 'album'
+]
+
+
+SMTP_USERNAME = GOOGLE_USERNAME
+SMTP_PASSWORD = GOOGLE_PASSWORD
+SMTP_HOSTNAME = "smtp.gmail.com"
+SMTP_PORT = 587
+
+SMTP_TO_ADDR = ''
+SMTP_TO_SUBJECT = "GPM - Playlist Diff"
+
+
+import csv
+import os
+import sys
+
+from gmusicapi import Mobileclient as GpmClient
+
+from git import Repo as GitRepo
+from git.exc import InvalidGitRepositoryError, NoSuchPathError
+
+from pprint import pprint
+
+from pygments import highlight
+from pygments.lexers import DiffLexer
+from pygments.formatters import HtmlFormatter
+
+from smtplib import SMTP
+
+
+def git_initialise():
+    try:
+        # attempt to open existing repository
+        repository = GitRepo(GIT_REPO_PATH)
+        print(f"Opened existing repository in '{GIT_REPO_PATH}'")
+    except (InvalidGitRepositoryError, NoSuchPathError):
+        # initialise bare repository
+        os.makedirs(GIT_REPO_PATH, exist_ok=True)
+        repository = GitRepo.init(GIT_REPO_PATH)
+        print(f"Initialised new repository in '{GIT_REPO_PATH}'")
+    
+    return repository
+
+
+def git_clean_repo(repository):
+    # check if repository is dirty
+    if repository.head.is_valid():
+        if repository.is_dirty():
+            print("Repository is dirty")
+            print("Attempting hard reset to HEAD")
+            repository.head.reset(working_tree=True)
+            print("Hard reset successful")
+
+
+def git_checkout(repository):
+    # check for requested branch
+    if repository.active_branch.name != GIT_REPO_BRANCH:
+        print(f"HEAD is on branch '{repository.active_branch.name}'")
+        print(f"Checking out branch '{GIT_REPO_BRANCH}'")
+        repository.git.checkout('-b', GIT_REPO_BRANCH)
+        print("Checked out requested branch")
+
+
+def git_changes_made(repository):
+    return repository.is_dirty() or len(repository.index.diff(None))
+
+
+def gpm_initialise():
+    return GpmClient(debug_logging=True)
+
+
+def gpm_authenticate(client):
+    print("Attempting to authenticate with Google Play Music")
+
+    client.login(
+        GOOGLE_USERNAME,
+        GOOGLE_PASSWORD,
+        GOOGLE_DEVICE_ID,
+        locale=GOOGLE_LOCALE
+    )
+
+    return client.is_authenticated()
+
+def gpm_build_track_cache(tracks):
+    return {track['id']:track for track in tracks}
+
+
+def gpm_build_track_entry(track):
+    return {
+        'title': track.get('title', ''),
+        'artist': track.get('artist', ''),
+        'album': track.get('album', '')
+    }
+
+
+def gpm_build_playlist_file_name(name):
+    return name.replace('/', '-') + '.csv'
+
+
+def gpm_get_playlists(client):
+    return sorted(
+        client.get_all_user_playlist_contents(),
+        key=lambda playlist: playlist['name']
+    )
+
+
+def gpm_get_tracks(client):
+    return client.get_all_songs()
+
+
+def gpm_resolve_track(entry, cache):
+    if 'track' in entry:
+        return entry['track']
+    elif 'trackId' in entry:
+        if entry['trackId'] in cache:
+            return cache[entry['trackId']]
+
+    return None
+
+
+def smtp_authenticate():
+    server = SMTP(SMTP_HOSTNAME, SMTP_PORT)
+    server.ehlo()
+    server.starttls()
+    server.login(GOOGLE_USERNAME, GOOGLE_PASSWORD)
+
+    return server
+
+
+def main():
+    # git initialisation
+    repository = git_initialise()
+    git_clean_repo(repository)
+    git_checkout(repository)
+
+
+    # gpm initialisation
+    client = gpm_initialise()
+    if not gpm_authenticate(client):
+        print('Authentication failed.')
+        sys.exit(1)
+
+    # gpm api calls
+    playlists = gpm_get_playlists(client)
+    tracks = gpm_get_tracks(client)
+
+    # build gpm track cache
+    cache = gpm_build_track_cache(tracks)
+
+
+    # playlist diffs
+    diffs = []
+
+
+    # iterate through playlists
+    for playlist in playlists:
+        # build file name + path
+        file_name = gpm_build_playlist_file_name(playlist['name'])
+        file_path = os.path.join(GIT_REPO_PATH, file_name)
+        
+        # create / open playlist file for writing
+        with open(file_path, 'w', newline='') as csv_file:
+            print('Exporting playlist "' + playlist['name'] + '"... ')
+
+            # write CSV file header
+            csv_writer = csv.DictWriter(csv_file, GPM_TRACK_FIELDS)
+            csv_writer.writeheader()
+
+            # iterate through playlist entries
+            for entry in playlist['tracks']:
+                # resolve track entry, build output
+                track = gpm_resolve_track(entry, cache)
+                output = gpm_build_track_entry(track)
+
+                # write track output to disk
+                if track is not None:
+                    csv_writer.writerow(output)
+
+                
+            print('\tWritten to disk.')
+
+        # calculate playlist diff
+        diff = repository.index.diff(None, create_patch=True, paths=file_path)
+
+        # check for tracking status
+        changed = bool(len(diff))
+        untracked = file_name in repository.untracked_files
+
+        # check for playlist changes
+        if changed:
+            # append to global diffs list
+            print('\tCalculating diff...')
+            diffs += diff
+            print('\tDiff appended.')
+
+        # check for committable diff
+        if changed or untracked:
+            # stage playlist file
+            print('\tStaging "' + file_path + '"... ')
+            repository.index.add([file_path])
+            print('\tStaged.')
+
+
+    # check for playlist changes
+    if git_changes_made(repository):
+        # commit staged playlist changes
+        print('Committing to repository... ', end='')
+        #repository.index.commit('Playlist changes')
+        print('done!')
+
+        # build pretty diff
+        # pretty_diff = ""
+        # pretty_diff += HtmlFormatter().get_style_defs('.highlight')
+        # pretty_diff += "\n<br />\n".join(
+        #     highlight(
+        #         str(diff),
+        #         DiffLexer(),
+        #         HtmlFormatter()
+        #     ) for diff in diffs
+        # )
+
+        # build SMTP output
+        # body = "\r\n".join([
+        #     "From: %s" % SMTP_USERNAME,
+        #     "To: %s" % SMTP_TO_ADDR,
+        #     "Subject: %s" % SMTP_TO_SUBJECT,
+        #     "MIME-Version: 1.0",
+        #     "Content-Type: text/html",
+        #     "",
+        #     pretty_diff
+        # ])
+
+        # print(body)
+        # print("")
+        # print(diffs)
+        # print(pretty_diff)
+
+        # initialise SMTP mailer
+        # mailer = smtp_authenticate()
+        # mailer.sendmail(SMTP_USERNAME, SMTP_TO_ADDR, body)
+        # mailer.quit()
+    else:
+        print('No diff to commit.')
+        return
+
+main()
